d939944323fb1336e33c4196735243dcdbb971b1
rcdavis4 <rcdavis4@gmail.com>
2015-04-25 10:18:13 -0700

    use strict

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,40 @@
+"use strict";
+
 $(function(){
 
+  $('select').material_select();
+
+  $.getJSON('http://omdbapi.com/?s=star%20wars&r=json', function (data) {
+    var myData = data.Search;
+    displayResults(myData);
+  });
+
+//  function displayResults(results) {
+//    $.each(results, function(index, value) {
+//        console.log(value.Title);
+//      $('#search-results').append('<br>' + value.Title + '<br>');
+//    });
+//  }
+
+//  function displayResults(results) {
+//    var html = "";
+//    $.each(results, function(index, value) {
+//      html += '<p>' + value.Title + '</p>';
+//      console.log(value.Title); // to make sure data is coming in
+//    });
+//
+//    $('#search-results').html(html);
+//  }
+
+  function displayResults(results) {
+    var html = [];
+
+    for (var x in results) {
+      html.push('<p>' + results.Title + '</p>');
+    };
+
+    $('#search-results').innerHTML = html.join('');
+  }
 
 
 
